296cf4cf94ee7d0b88be3cad4b8ace94a7c32240
catatsuy <catatsuy@catatsuy.org>
2014-10-19 16:59:32 +0900

    3台にデプロイする

--- final/webapp/go/deploy.sh
+++ final/webapp/go/deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -e -x
 
 export SSH_KEYFILE="${SSH_KEYFILE:-$HOME/.ssh/isucon2014-09-23.pem}"
-export SSH_SERVER=isucon@54.64.166.133
+export SSH_USER=isucon
 export RSYNC_RSH="ssh -i $SSH_KEYFILE"
 
 curl --data "deploy by $USER" 'https://teamfreesozai.slack.com/services/hooks/slackbot?token=oxjd47qGfo59VhemVz43FQZF&channel=%23general'
-rsync -avz ./ $SSH_SERVER:/home/isucon/isucon3/final/webapp/go/
+
+SERVERS="54.64.230.18 54.64.166.133 54.64.226.134"
+
+for SERVER in ${SERVERS}; do
+  rsync -avz ./ ${SSH_USER}@${SERVER}:/home/isucon/isucon3/final/webapp/go/
+done
